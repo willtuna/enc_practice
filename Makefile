71b8582ecdc45ce76fc1c847358c4e19876dd93a
willvegapunk <willvegapunk@gmail.com>
2018-06-25 21:30:43 +0800

    Integraet text2trit with main code

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=gcc
-CFLAGS=-WALL -g
+CFLAGS=-Wall -g
 
 all: 
-	$(CC) -o ntru_c.out    ./ntru_encrypt_v2.c
-	$(CC) -o text2trit.out ./text2trit.c
+	$(CC) -o ntru_c.out    $(CFLAGS) ./ntru_encrypt_v3.c ./bit2tritCon/char2trit_lib.o
+	$(CC) -o text2trit.out $(CFLAGS) ./text2trit.c
---
+++ bit2tritCon/bit2trit.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef BIT2TRIT_H
+#define BIT2TRIT_H
+
+#define NUM_TRITS 41
+
+typedef struct trits {
+    int trit_poly [NUM_TRITS] ;
+} Message;
+
+int char2trit(char * infile_path, Message *const msg_arr);
+int trit2char(Message *const decrypted_msg_arr, int num_block);
+void read_tritfile(FILE *ptr);
+
+#endif
+
